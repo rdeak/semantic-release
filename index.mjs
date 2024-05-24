e4976b646ad8b51b615045893f61a6edd8ef1f46
Robert Deak <robert.deak@gmail.com>
2024-05-24 11:17:57 +0200

    feat: fix flow
    Signed-off-by: Robert Deak <robert.deak@gmail.com>

--- index.mjs
+++ index.mjs
@@ -1,37 +1,49 @@
 import core from "@actions/core";
 import semanticRelease from "semantic-release";
+import {spawnSync} from "node:child_process";
 
 async function run() {
-  try {
-    console.log(JSON.stringify(process.env, null, 2))
-    const npmToken = process.env.NPM_TOKEN;
-    const extendConfig = `@rdeak/semantic-release-config${npmToken ? "" : "/release-only"}`;
+    try {
+        const spawn = spawnSync('git', [
+            'config',
+            '--global',
+            '--add',
+            'safe.directory',
+            process.env.GITHUB_WORKSPACE,
+        ]);
+        if (spawn.status !== 0) {
+            core.setFailed(`Can't set safe dir: ${ spawn.stderr }`);
+            return;
+        }
 
-    const result = await semanticRelease({
-      extends: extendConfig,
-    });
+        const npmToken = process.env.NPM_TOKEN;
+        const extendConfig = `@rdeak/semantic-release-config${npmToken ? "" : "/release-only"}`;
 
-    if (!result) {
-      core.exportVariable('NEW_RELEASE_CREATED', "false");
-      core.info("No release created");
-      return;
+        const result = await semanticRelease({
+            extends: extendConfig,
+        });
+
+        if (!result) {
+            core.exportVariable('NEW_RELEASE_CREATED', "false");
+            core.info("No release created");
+            return;
+        }
+
+        const {version, gitTag} = result.nextRelease;
+        const [majorVersion] = version.split(".");
+
+        core.exportVariable('NEW_RELEASE_CREATED', "true");
+        core.exportVariable("RELEASE_VERSION", version);
+        core.exportVariable("RELEASE_MAJOR_VERSION", majorVersion);
+        core.exportVariable("RELEASE_TAG", gitTag);
+
+        core.setOutput("new_release_created", "true");
+        core.setOutput("release_version", version);
+        core.setOutput("release_major_version", majorVersion);
+        core.setOutput("release_tag", gitTag);
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    const { version, gitTag } = result.nextRelease;
-    const [majorVersion] = version.split(".");
-
-    core.exportVariable('NEW_RELEASE_CREATED', "true");
-    core.exportVariable("RELEASE_VERSION", version);
-    core.exportVariable("RELEASE_MAJOR_VERSION", majorVersion);
-    core.exportVariable("RELEASE_TAG", gitTag);
-
-    core.setOutput("new_release_created", "true");
-    core.setOutput("release_version", version);
-    core.setOutput("release_major_version", majorVersion);
-    core.setOutput("release_tag", gitTag);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 }
 
 run();
